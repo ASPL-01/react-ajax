4c1832895547bb89d08cb78f95254eef8c6cf4bb
Chyld Medford <chyld.medford@gmail.com>
2017-01-10 11:56:23 +0530

    fin

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import Home from './components/Home';
 import About from './components/About';
 import Faq from './components/Faq';
+import Boxes from './components/boxes/Boxes';
+import Random from './components/Random';
 
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
+      <Route path="random" component={Random} />
+      <Route path="boxes" component={Boxes} />
       <Route path="about" component={About} />
       <Route path="faq" component={Faq} />
     </Route>
