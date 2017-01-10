1223c11a4cd38b3119c3e28377f657c2e9c946d4
Chyld Medford <chyld.medford@gmail.com>
2017-01-10 15:38:05 +0530

    wip

--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -4,7 +4,23 @@ import axios from 'axios';
 export default class Movies extends React.Component{
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {results: []};
+    this.search = this.search.bind(this);
+    this.clear = this.clear.bind(this);
+  }
+
+  clear(){
+    this.setState({results: []});
+  }
+
+  search(){
+    const query = this.query.value;
+    const url = `http://www.omdbapi.com/?s=${query}&page=1`;
+    axios.get(url)
+    .then(rsp => {
+      const results = rsp.data.Search;
+      this.setState({results});
+    });
   }
 
   render(){
@@ -14,9 +30,34 @@ export default class Movies extends React.Component{
         <div className="panel panel-default">
           <div className="panel-body">
             <label>Search</label>
-            <input ref={n => this.query = n} type="number" />
+            <input ref={n => this.query = n} type="text" />
             <button onClick={this.search} className="btn btn-primary btn-sm">Search</button>
+            <button onClick={this.clear} className="btn btn-danger btn-sm">Clear</button>
           </div>
+        </div>
+        <div>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Year</th>
+                <th>Poster</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                this.state.results.map((r, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>{r.Title}</td>
+                      <td>{r.Year}</td>
+                      <td><img src={r.Poster}/></td>
+                    </tr>
+                  );
+                })
+              }
+            </tbody>
+          </table>
         </div>
       </div>
     );
